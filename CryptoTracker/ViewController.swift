fc725de3a636d38be94c6bbb680993fada2ab3ef
Vigneshraj Sekar Babu <vigneshrajsekarbabu@vigneshrajs-macbook-pro.local>
2018-06-18 20:57:13 -0500

    Initial Commit

---
+++ CryptoTracker/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CryptoTracker
+//
+//  Created by Vigneshraj Sekar Babu on 6/18/18.
+//  Copyright © 2018 Vigneshraj Sekar Babu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
